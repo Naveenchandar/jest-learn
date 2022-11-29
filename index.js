161cb6b40b9b79497b57b2bb153f735395ecc7aa
Naveenchandar <naveenchandece@gmail.com>
2022-11-29 16:50:25 +0530

    same file with sample function

---
+++ index.js
@@ -0,0 +1,18 @@
+function additon(a, b) {
+    return a + b
+}
+
+function subtraction(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+module.exports = { additon, subtraction, multiply, divide };
+
