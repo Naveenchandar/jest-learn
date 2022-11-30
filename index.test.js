acd68e0b825e0e5ec7645c6a05e564ccee80aa63
Naveenchandar <naveenchandece@gmail.com>
2022-11-30 14:36:09 +0530

    truthy values test, primitive types test and non primitive types test

--- index.test.js
+++ index.test.js
@@ -65,3 +65,108 @@ test('Should subtract and return only positive number', () => {
     expect(result).toBe(10);
     expect(result).not.toBe(-10); // This will fail
 });
+
+test('primitive types test', () => {
+    expect('1').toBe('1');
+    expect(1).toBe(1);
+    expect(true).toBe(true);
+    expect(null).toBe(null);
+    expect(undefined).toBe(undefined);
+});
+
+test('non primitive type test', () => {
+    expect([]).toEqual([]);
+    expect({}).toEqual({});
+});
+
+test('primitive types test with toEqual', () => {
+    expect('1').toEqual('1');
+    expect(1).toEqual(1);
+    expect(true).toEqual(true);
+    expect(null).toEqual(null);
+    expect(undefined).toEqual(undefined);
+});
+
+test('non primitive type test with toBe', () => {
+    expect([]).toBe([]);
+    expect({}).toBe({});
+});
+
+test('Numbers test', () => {
+    const result = 1 + 2 + 3;
+    expect(result).toBe(6);
+    expect(result).toEqual(6);
+})
+
+describe('String test', () => {
+    it('Ckeck regex contains string', () => {
+        expect('hello world').toMatch(/w/);
+    });
+});
+
+it('toEqual and toStrictEqual check for objects value undefined', () => {
+    const a = { a: undefined, b: 2 };
+    const b = { b: 2 };
+    expect(a).toStrictEqual(b);
+    expect(a).toEqual(b);
+});
+
+it('toEqual and toStrictEqual check for arrays', () => {
+    const a = [1, 2, 3, 4];
+    const b = [1, undefined, 3, 4];
+    expect(a).toStrictEqual(b); // This will fail
+    expect(a).toEqual(b); // This will fail
+    expect(a).toEqual([1, 2, 3, 4]);
+});
+
+it('toEqual and toStrictEqual check for objects value null', () => {
+    const a = { a: null, b: 2 };
+    const b = { b: 2 };
+    expect(a).toStrictEqual(b); // This will fail
+    expect(a).toEqual(b);
+});
+
+
+
+// Truthy values
+
+it('checks for null', () => {
+    const result = null;
+    expect(result).toBeNull();
+    expect(result).toBe(null);
+    expect(result).toEqual(null);
+});
+
+it('checks for undefined', () => {
+    const result = undefined;
+    expect(result).toBeUndefined();
+    expect(result).toBe(undefined);
+    expect(result).toEqual(undefined);
+});
+
+it('checks for defined. ', () => {
+    const result = 1;
+    expect(result).toBeNull(); // This will fail
+    expect(result).toBeUndefined(); // This will fail
+    expect(result).toBeDefined();
+    expect(result).toBe(undefined); // This will fail
+    expect(result).toEqual(undefined); // This will fail
+});
+
+test('null', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined(); // This will fail
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+});
+
+test('zero', () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+});
